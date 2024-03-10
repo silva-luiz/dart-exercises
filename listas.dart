0602948a03a053928a169acf25045c93df215614
Luiz Henrique Gomes da Silva <luiz.hgsilva@hotmail.com>
2024-03-10 15:20:57 -0300

    first commit - some exercises added

---
+++ listas.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<String> familia = ['Luiz', 'Karla', 'Theo', 'Alice'];
+
+  print(familia[1]); // Pega o segundo elemento da lista -> Karla
+  print(familia.length);
+  print(familia.first);
+}
---
+++ variaveis.dart
@@ -0,0 +1,16 @@
+void main() {
+  int idade = 28;
+  double altura = 1.81;
+  bool dev = true;
+  String nome = 'Luiz Henrique';
+
+  print('meu nome é $nome,'
+      'tenho $idade anos.'
+      'tenho $altura m de altura.'
+      'Trabalho como dev? $dev');
+
+  print(nome);
+  print(idade);
+  print(altura);
+  print(dev);
+}
